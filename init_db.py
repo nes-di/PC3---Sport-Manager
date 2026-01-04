8951160d8f25ea48f6beb00ad7a72f9ebc71888e
sabri <younes.sabri670@gmail.com>
2026-01-04 15:22:17 +0100

    Ajout de 18 joueurs par équipe (11 titulaires + 7 remplaçants)

--- init_db.py
+++ init_db.py
@@ -46,23 +46,33 @@ def insert_seed_data():
     cursor.execute("SELECT id FROM Equipe WHERE nom = 'PSG'")
     id_psg = cursor.fetchone()[0]
     
-    # Insertion des joueurs de l'OL (Mon équipe) - 11 joueurs
+    # Insertion des joueurs de l'OL (Mon équipe) - 18 joueurs (11 titulaires + 7 remplaçants)
     joueurs_ol = [
+        # === TITULAIRES (11) ===
         # Gardien
-        ("Olivier", "Olivier", "Gardien", 15, 5, 70, 10),
+        ("Lopes", "Anthony", "Gardien", 15, 50, 70, 45),
         # Défenseurs
-        ("Mehdi", "Mehdi", "Défenseur Central", 10, 40, 40, 10),
-        ("Paul", "Paul", "Défenseur Central", 10, 40, 40, 10),
-        ("Thomas", "Thomas", "Défenseur Latéral Droit", 45, 35, 25, 15),
-        ("Kevin", "Kevin", "Défenseur Latéral Gauche", 40, 40, 30, 10),
+        ("Lukeba", "Castello", "Défenseur Central", 25, 55, 65, 35),
+        ("Tagliafico", "Nicolas", "Défenseur Central", 20, 60, 70, 40),
+        ("Gusto", "Malo", "Défenseur Latéral Droit", 60, 55, 45, 50),
+        ("Henrique", "Alex", "Défenseur Latéral Gauche", 55, 60, 50, 45),
         # Milieux
-        ("Abdel", "Abdel", "Milieu Défensif", 5, 50, 10, 35),
-        ("Antoine", "Antoine", "Milieu Central", 20, 45, 15, 40),
-        ("Maxime", "Maxime", "Milieu Offensif", 30, 30, 10, 50),
+        ("Thuram", "Khéphren", "Milieu Défensif", 40, 70, 55, 60),
+        ("Caqueret", "Maxence", "Milieu Central", 45, 65, 40, 70),
+        ("Tolisso", "Corentin", "Milieu Offensif", 35, 60, 50, 75),
         # Attaquants
-        ("Evann", "Evann", "Ailier Gauche", 65, 5, 5, 25),
-        ("Rayan", "Rayan", "Ailier Droit", 60, 10, 10, 30),
-        ("Emre", "Emre", "Attaquant Centre", 10, 10, 30, 50)
+        ("Cherki", "Rayan", "Ailier Gauche", 70, 50, 30, 80),
+        ("Fofana", "Malick", "Ailier Droit", 75, 55, 35, 70),
+        ("Lacazette", "Alexandre", "Attaquant Centre", 40, 60, 60, 85),
+        
+        # === REMPLAÇANTS (7) ===
+        ("Perri", "Lucas", "Gardien", 10, 45, 65, 35),
+        ("Mata", "Clinton", "Défenseur", 30, 50, 55, 30),
+        ("Diomandé", "Sinaly", "Défenseur", 25, 45, 60, 25),
+        ("Lepenant", "Johann", "Milieu", 30, 55, 40, 50),
+        ("Maitland-Niles", "Ainsley", "Milieu", 50, 50, 35, 55),
+        ("Nuamah", "Ernest", "Attaquant", 65, 40, 25, 60),
+        ("Mikautadze", "Georges", "Attaquant", 35, 45, 50, 65)
     ]
     
     for nom, prenom, poste, vitesse, endurance, force, technique in joueurs_ol:
@@ -71,23 +81,33 @@ def insert_seed_data():
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0)
         """, (id_ol, nom, prenom, poste, vitesse, endurance, force, technique))
     
-    # Insertion des joueurs du PSG (Équipe adverse) - 11 joueurs
+    # Insertion des joueurs du PSG (Équipe adverse) - 18 joueurs (11 titulaires + 7 remplaçants)
     joueurs_psg = [
+        # === TITULAIRES (11) ===
         # Gardien
-        ("Victor", "Victor", "Gardien", 15, 5, 70, 10),
+        ("Donnarumma", "Gianluigi", "Gardien", 20, 55, 75, 50),
         # Défenseurs
-        ("Sergio", "Sergio", "Défenseur Central", 15, 45, 45, 15),
-        ("Marquinhos", "Marquinhos", "Défenseur Central", 20, 40, 40, 20),
-        ("Achraf", "Achraf", "Défenseur Latéral Droit", 70, 30, 20, 20),
-        ("Nuno", "Nuno", "Défenseur Latéral Gauche", 50, 35, 25, 10),
+        ("Marquinhos", "Marcos", "Défenseur Central", 25, 65, 75, 55),
+        ("Skriniar", "Milan", "Défenseur Central", 20, 60, 80, 50),
+        ("Hakimi", "Achraf", "Défenseur Latéral Droit", 85, 60, 50, 65),
+        ("Mendes", "Nuno", "Défenseur Latéral Gauche", 60, 65, 55, 50),
         # Milieux
-        ("Marco", "Marco", "Milieu Défensif", 10, 50, 20, 40),
-        ("Vitinha", "Vitinha", "Milieu Central", 25, 40, 10, 50),
-        ("Warren", "Warren", "Milieu Offensif", 35, 25, 15, 55),
+        ("Vitinha", "Vítor", "Milieu Défensif", 45, 75, 45, 75),
+        ("Zaïre-Emery", "Warren", "Milieu Central", 50, 70, 40, 80),
+        ("Ruiz", "Fabián", "Milieu Offensif", 40, 65, 50, 85),
         # Attaquants
-        ("Noah", "Noah", "Ailier Gauche", 65, 5, 5, 25),
-        ("Ousmane", "Ousmane", "Ailier Droit", 80, 20, 10, 40),
-        ("Lucas", "Lucas", "Attaquant Centre", 10, 10, 30, 50)
+        ("Dembélé", "Ousmane", "Ailier Gauche", 90, 60, 40, 85),
+        ("Barcola", "Bradley", "Ailier Droit", 85, 65, 45, 75),
+        ("Gonçalo Ramos", "Gonçalo", "Attaquant Centre", 50, 65, 70, 80),
+        
+        # === REMPLAÇANTS (7) ===
+        ("Navas", "Keylor", "Gardien", 15, 50, 70, 45),
+        ("Mukiele", "Nordi", "Défenseur", 55, 60, 60, 40),
+        ("Beraldo", "Lucas", "Défenseur", 30, 55, 65, 35),
+        ("Ugarte", "Manuel", "Milieu", 40, 70, 60, 60),
+        ("Soler", "Carlos", "Milieu", 35, 60, 45, 70),
+        ("Asensio", "Marco", "Attaquant", 55, 55, 40, 80),
+        ("Kolo Muani", "Randal", "Attaquant", 65, 60, 65, 70)
     ]
     
     for nom, prenom, poste, vitesse, endurance, force, technique in joueurs_psg:
