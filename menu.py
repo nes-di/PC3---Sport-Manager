edc6f7d4658e251d516f6513fa86d79ba9e03dad
sabri <younes.sabri670@gmail.com>
2026-01-04 15:14:54 +0100

    Création du module menu pour l'interface utilisateur

---
+++ menu.py
@@ -0,0 +1,19 @@
+"""
+Module de gestion du menu principal
+"""
+
+
+def afficher_menu():
+    """
+    Affiche le menu principal de l'application
+    """
+    print("\n" + "=" * 50)
+    print("          SPORT MANAGER - MENU PRINCIPAL")
+    print("=" * 50)
+    print("1. Afficher mon équipe")
+    print("2. Ajouter un joueur")
+    print("3. Jouer un match")
+    print("4. Provoquer une blessure (TEST)")
+    print("5. Afficher l'historique des matchs")
+    print("6. Quitter")
+    print("=" * 50)
